2839f69b76fbd7ef4607b67a074f7c19c3268bdb
Dn Hailemariam Eyayu <hailemariameyayu2012@gmail.com>
2025-08-07 01:46:08 +0300

    updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://eden:eden@cluster0.vjpxkb6.mongodb.net/?', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("DB Connected (Local)");
-    } catch (error) {
-        console.error("MongoDB Connection Error: ", error.message);
-    }
+  try {
+    await mongoose.connect('mongodb+srv://eden:eden@cluster0.vjpxkb6.mongodb.net/?', {
+    // await mongoose.connect("mongodb://127.0.0.1:27017/food-del", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB Connected (Local)");
+  } catch (error) {
+    console.error("MongoDB Connection Error: ", error.message);
+  }
 };
