9af182f56956f2344d5473169cf33184e13de470
Dn Hailemariam Eyayu <hailemariameyayu2012@gmail.com>
2025-08-07 02:04:55 +0300

    up

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://eden:eden@cluster0.vjpxkb6.mongodb.net/?', {
+    await mongoose.connect('mongodb+srv://eden:<db_password>@cluster0.vjpxkb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
     // await mongoose.connect("mongodb://127.0.0.1:27017/food-del", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
